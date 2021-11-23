6da0785ee88f22f3eae5d08f71083a6b0a263f2f
Tadashi Konno <jensonnumber1@gmail.com>
2021-11-22 20:40:51 -0800

    create project view extends layout and is styled

--- resources/views/create-project.blade.php
+++ resources/views/create-project.blade.php
@@ -1,6 +1,18 @@
-<form action="{{route('project.store')}}" method="POST">
-  @csrf
-  <label for="name">project name</label>
-  <input type="text" id="name" name="name" placeholder="project A">
-  <button type="submit">create this project !</button>
-</form>
+@extends('layouts.app')
+
+@section('title', 'create project')
+
+@section('content')
+<div class="container pt-3">
+  <form action="{{route('project.store')}}" method="POST">
+    @csrf
+    <div class="form-group">
+      <label for="name">project name</label>
+      <input class="form-control" type="text" id="name" name="name" placeholder="project A">
+    </div>
+    
+    <button class="btn btn-primary" type="submit">create this project !</button>
+  </form>
+</div>
+
+@endsection
